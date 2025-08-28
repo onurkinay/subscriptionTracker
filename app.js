3564105fcecf0a35bb227d38617052469040e50f
Onur KINAY <ben@onurkinay.com.tr>
2025-08-28 16:31:16 +0300

    error handling middleware

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import routes from "./routes/_index.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1", routes);
+
+// Error handling middleware
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api/v1", routes);
-
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
-
   await connectToDatabase();
 });
 
